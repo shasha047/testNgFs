8fa0c3d81339e9a90a106a7e03e30f1f0008e872
shasha047 <shasha09047@gmail.com>
2017-04-10 02:04:59 +0530

    remove thing endpoint, add game endpoint

--- server/api/game/game.socket.js
+++ server/api/game/game.socket.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-import ThingEvents from './thing.events';
+import GameEvents from './game.events';
 
 // Model events to emit
 var events = ['save', 'remove'];
@@ -13,9 +13,9 @@ export function register(socket) {
   // Bind model events to socket events
   for(var i = 0, eventsLength = events.length; i < eventsLength; i++) {
     var event = events[i];
-    var listener = createListener(`thing:${event}`, socket);
+    var listener = createListener(`game:${event}`, socket);
 
-    ThingEvents.on(event, listener);
+    GameEvents.on(event, listener);
     socket.on('disconnect', removeListener(event, listener));
   }
 }
@@ -29,6 +29,6 @@ function createListener(event, socket) {
 
 function removeListener(event, listener) {
   return function() {
-    ThingEvents.removeListener(event, listener);
+    GameEvents.removeListener(event, listener);
   };
 }
--- server/config/socketio.js
+++ server/config/socketio.js
@@ -16,6 +16,7 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
+  require('../api/game/game.socket').register(socket);
   require('../api/thing/thing.socket').register(socket);
 }
 
